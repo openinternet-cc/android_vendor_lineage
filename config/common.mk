74c789a1c597f3c9b4e668e9b05eb2f6c2bf4979
g <gWOLF3@protonmail.com>
2020-12-08 00:08:39 -0600

    import lawnchair

--- config/common.mk
+++ config/common.mk
@@ -311,3 +311,7 @@ endif
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
+
+
+# try to import lawnchair?
+$(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
